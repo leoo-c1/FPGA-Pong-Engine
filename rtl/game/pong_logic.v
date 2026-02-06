fde35e893364743804940048a061ba33f0809326
leoo-c1 <chinchillaleo01@gmail.com>
2026-02-06 14:15:22 +1100

    Fix syntax errors

--- rtl/game/pong_logic.v
+++ rtl/game/pong_logic.v
@@ -31,7 +31,7 @@ module pong_logic (
     output reg sq_missed = 1'b1,    // If the we miss the square and it hits the left/right side
 
     // Square collision point
-    output reg [6:0] hit_y,         // The distance from paddle centre to the square during a hit
+    output reg [6:0] hit_y          // The distance from paddle centre to the square during a hit
     );
 
     parameter h_video = 640;        // Horizontal active video (in pixels)
@@ -207,24 +207,24 @@ module pong_logic (
                         sq_ypos == pdl2_ypos + pdl_height - 1) begin
                         sq_yveldir <= ~sq_yveldir;  // Change direction along y-axis
                         sq_ypos <= sq_ypos + 1;     // Move down one pixel
-                        hit_y <= pdl_height/2
+                        hit_y <= pdl_height/2;
                     
                     // Check if bottom of the square is hitting the top of the paddle
                     end else if (sq_ypos + sq_width == pdl2_ypos || 
                                 sq_ypos + sq_width == pdl2_ypos + 1) begin
                         sq_yveldir <= ~sq_yveldir;  // Change direction along y-axis
                         sq_ypos <= sq_ypos - 1;     // Move up by one pixel
-                        hit_y <= pdl_height/2
+                        hit_y <= pdl_height/2;
 
                     end else begin
                         sq_xveldir <= ~sq_xveldir;  // Change direction along x-axis
                         sq_xpos <= sq_xpos - 1;     // Move to the left by one pixel
                         // Check if the square hits below the paddle's centre
-                        if (sq_ypos >= padl2_ypos + pdl_height/2) begin
-                            hit_y <= sq_ypos - padl2_ypos - pdl_height/2 + 1;
+                        if (sq_ypos >= pdl2_ypos + pdl_height/2) begin
+                            hit_y <= sq_ypos - pdl2_ypos - pdl_height/2 + 1;
                             sq_yveldir <= 1'b1;
                         end else begin // If we are at/above the paddle's centre
-                            hit_y <= padl2_ypos + pdl_height/2 - sq_ypos - sq_width + 1;
+                            hit_y <= pdl2_ypos + pdl_height/2 - sq_ypos - sq_width + 1;
                             sq_yveldir <= 1'b0;
                         end
                     end
@@ -242,24 +242,24 @@ module pong_logic (
                         sq_ypos == pdl1_ypos + pdl_height - 1) begin
                         sq_yveldir <= ~sq_yveldir;  // Change direction along y-axis
                         sq_ypos <= sq_ypos + 1;     // Move down one pixel
-                        hit_y <= pdl_height/2
+                        hit_y <= pdl_height/2;
                     
                     // Check if bottom of the square is hitting the top of the paddle
                     end else if (sq_ypos + sq_width == pdl1_ypos || 
                                 sq_ypos + sq_width == pdl1_ypos + 1) begin
                         sq_yveldir <= ~sq_yveldir;  // Change direction along y-axis
                         sq_ypos <= sq_ypos - 1;     // Move up by one pixel
-                        hit_y <= pdl_height/2
+                        hit_y <= pdl_height/2;
 
                     end else begin
                         sq_xveldir <= ~sq_xveldir;  // Change direction along y-axis
                         sq_xpos <= sq_xpos + 1;     // Move to the right one pixel
                         // Check if the square hits below the paddle's centre
-                        if (sq_ypos >= padl1_ypos + pdl_height/2) begin
-                            hit_y <= sq_ypos - padl1_ypos - pdl_height/2 + 1;
+                        if (sq_ypos >= pdl1_ypos + pdl_height/2) begin
+                            hit_y <= sq_ypos - pdl1_ypos - pdl_height/2 + 1;
                             sq_yveldir <= 1'b1;
                         end else begin // If we are at/above the paddle's centre
-                            hit_y <= padl1_ypos + pdl_height/2 - sq_ypos - sq_width + 1;
+                            hit_y <= pdl1_ypos + pdl_height/2 - sq_ypos - sq_width + 1;
                             sq_yveldir <= 1'b0;
                         end
                     end
--- rtl/game/velocity_mapper.v
+++ rtl/game/velocity_mapper.v
@@ -12,21 +12,21 @@ module velocity_mapper #(
     input game_startup,         // Whether or not the game is on the startup menu
 
     output reg [8:0] sq_xvel,   // Squares's horizontal velocity in pixels/second
-    output reg [8:0] sq_yvel,   // Squares's vertical velocity in pixels/second
+    output reg [8:0] sq_yvel    // Squares's vertical velocity in pixels/second
     );
 
     parameter pdl_height = 96;
 
     parameter SCALE_VELX = 2 * (MAX_VEL - MIN_VEL) / pdl_height;
-    parameter SCALE_VELY = 2 * MAX_VEL / pdl_height
+    parameter SCALE_VELY = 2 * MAX_VEL / pdl_height;
 
     always @ (posedge clk_0) begin
         if (!rst) begin         // If we reset, set velocities to default velocity
-            sq_xvel <= DEFAULT_VEL;
-            sq_yvel <= DEFAULT_VEL;
+            sq_xvel <= MIN_VEL;
+            sq_yvel <= MIN_VEL;
         end else if (sq_missed | game_over | game_startup) begin
-            sq_xvel <= DEFAULT_VEL;
-            sq_yvel <= DEFAULT_VEL;
+            sq_xvel <= MIN_VEL;
+            sq_yvel <= MIN_VEL;
         end else begin
             sq_xvel <= SCALE_VELX * hit_y + MIN_VEL;
             sq_yvel <= SCALE_VELY * hit_y;
